a29bb3653418ea7111a74c0e489dc468bfb88e80
unknown <purvi.harniya@gmail.com>
2021-06-22 16:27:16 +0530

    deploying

---
+++ build/precache-manifest.a3e0e2ae79542c4a38069ad3f9c3b6ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e95384cff88690d8047ba44e83a17b9",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "5d272b80f94baac4c379",
+    "url": "/covidtracker/static/css/main.018b5bb2.chunk.css"
+  },
+  {
+    "revision": "e62f851130d36e9bf740",
+    "url": "/covidtracker/static/js/2.b5d48125.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.b5d48125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d272b80f94baac4c379",
+    "url": "/covidtracker/static/js/main.103f06b9.chunk.js"
+  },
+  {
+    "revision": "de413a1930c6a774ec79",
+    "url": "/covidtracker/static/js/runtime-main.09cab292.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/covidimg.d7265326.png"
+  }
+]);
